76498ab7350e6d86f64bb875f1ba851e05294543
Thorsten-K <t.kisner@web.de>
2023-09-22 06:39:02 +0200

    logging activated

--- src/main/java/de/kisner/hibernate6/H6Test.java
+++ src/main/java/de/kisner/hibernate6/H6Test.java
@@ -1,8 +1,32 @@
 package de.kisner.hibernate6;
 
-import org.jeesl.interfaces.facade.JeeslFacade;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public interface RmmvFacade
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class H6Test
 {
+	final static Logger logger = LoggerFactory.getLogger(H6Test.class);
 	
+	public H6Test()
+	{
+		
+	}
+	
+	public void init()
+	{
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("h6");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+	}
+	
+	public static void main(String[] args)
+	{
+
+		logger.info("trace");
+
+	}
 }
